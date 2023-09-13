ea1bcd615cd49f13cbcee948769631f2d4a369aa
Harry Chen <czy88840616@gmail.com>
2023-09-13 19:11:21 +0800

    chore: update

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,9 +1,10 @@
 import { App, Configuration, ILifeCycle } from '@midwayjs/core';
 import { join } from 'path';
 import * as egg from '@midwayjs/web';
+import * as socketio from '@midwayjs/socketio';
 
 @Configuration({
-  imports: [egg],
+  imports: [egg, socketio],
   importConfigs: [join(__dirname, './config')],
 })
 export class MainConfiguration implements ILifeCycle {
